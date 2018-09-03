9981d93985041379cf6a1921615777ed44bbf6ae
Ryan Wiebe <wieberockin@yahoo.com>
2018-09-03 15:38:31 -0600

    Create a basic class and use it

--- SimpleClasses/Program.cs
+++ SimpleClasses/Program.cs
@@ -10,8 +10,48 @@ namespace SimpleClasses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            // new command uses the class pattern in memory to be usable by the application
+            Car myCar = new Car();
+            myCar.Make = "Oldsmobile";
+            myCar.Model = "Cutlass Supreme";
+            myCar.Year = 1986;
+            myCar.Color = "Silver";
+
+            Console.WriteLine("{0} {1} {2} {3}", 
+            myCar.Make, 
+            myCar.Model, 
+            myCar.Year, 
+            myCar.Color);
+
+            decimal value = DetermineMarketValue(myCar);
+            Console.WriteLine(value);
+
+
+            Console.ReadLine();
+
+        }
+
+        private static decimal DetermineMarketValue(Car car)
+        {
+            decimal carValue = 100.00M;
+            //Someday I might look up the car online
+            //to get a more accurate value
+
+            return carValue;
         }
     }
+
+    class Car
+    {
+
+        //Use the shortcut "prop" in intellisense and then hit Tab-Tab for the below.
+        //Another option would be propfull for more complete properties
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+
+    }
+
 }
